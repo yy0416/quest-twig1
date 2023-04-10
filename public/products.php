043c0168845cc66242754a1a7adc0e01b99a0710
yy0416 <wheathare@gmail.com>
2023-04-10 10:54:42 +0200

    fini

---
+++ public/products.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/../config/twig.php';
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+echo $twig->render('products.html.twig', ['products' => $products]);
